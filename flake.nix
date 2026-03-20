12d445048ab4c13601e67d1d4557c96e1a73ed71
Lennart Oldenburg <hello@lennartoldenburg.de>
2026-03-20 16:19:06 +0100

    Updated flake.nix to offer smoothly running Rust dev shell.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,4 @@
 {
-  description = "Create development shell with `nix develop`";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
@@ -21,17 +19,34 @@
     in
     {
       formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt-tree;
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default))
-          rust-analyzer
-          sqlx-cli
-          cargo-deny
-          pkg-config
-          openssl
-        ];
-        RUST_BACKTRACE = "full";
-        env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-      };
+      devShells.${system}.default =
+        let
+          rust-nightly = pkgs.rust-bin.selectLatestNightlyWith (
+            toolchain:
+            toolchain.default.override {
+              extensions = [
+                "rust-src"
+                "rust-analyzer"
+                "clippy"
+                "rustfmt"
+                "llvm-tools-preview"
+                "cargo"
+              ];
+            }
+          );
+        in
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            pkg-config
+            openssl
+            gcc
+            gnumake
+            rust-nightly
+            cargo-deny
+            sqlx-cli
+          ];
+          RUST_BACKTRACE = "full";
+          RUST_SRC_PATH = "${rust-nightly}/lib/rustlib/src/rust/library";
+        };
     };
 }
